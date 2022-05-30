4b5772f497e7db7a396d12c12fd27550b195aee5
omxmo <81746989+omxmo@users.noreply.github.com>
2022-05-30 18:56:08 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -15,9 +15,10 @@ ui.layout(
 					
 					<frame w="*" margin="10" h="1" bg="#ff8080" />
 					
-				    <button id="tmhb" text="每天最高领取天猫22888元红包（5月29日开始）" margin="10 3" textColor="#ffffff" bg="#ff5050"/>
-					<button id="jdhb" text="每天最高领取京东19618元红包（5月30日开始）" margin="10 3" textColor="#ffffff" bg="#ff5050"/>
-					<button id="youhuiquan" text="话费/电费充值91折（91元充值100元）" margin="10 3" textColor="#111111"/>
+				    <button id="tmhb" text="每天最高领取天猫22888元红包" margin="10 3" textColor="#ffffff" bg="#ff5050"/>
+					<button id="jdhb" text="每天最高领取京东19618元红包" margin="10 3" textColor="#ffffff" bg="#ff5050"/>
+					<button id="pddhb" text="拼多多iPhone直降千元，茅台直降600元" margin="10 3" textColor="#ffffff" bg="#ff5050"/>
+					<button id="youhuiquan" text="喵惠返现APP全网佣金最高，欢迎对比！" margin="10 3" textColor="#111111"/>
 					
 					<frame w="*" margin="10" h="1" bg="#ff8080" />
 					
@@ -32,7 +33,7 @@ ui.layout(
 				    <card id="qg_main" w="*" h="70" margin="10 5" cardCornerRadius="2dp" cardElevation="1dp" gravity="center_vertical">
                         <vertical padding="18 8" h="auto">
                             <text text="抢购功能" textColor="#222222" textSize="16sp"/>
-                            <text text="支持淘宝/某猫、某东、某多多和某音等平台" textColor="#999999" textSize="14sp"/>
+                            <text text="支持某宝/某猫、某东、某多多和某音等平台" textColor="#999999" textSize="14sp"/>
                         </vertical>
                     <View bg="#ff00ff" h="*" w="10"/>
                     </card>
@@ -63,25 +64,21 @@ ui.layout(
                 </frame>
                 <frame>
 				    <vertical>
-                     <text margin="10 5" w="*" gravity="center" textSize="18sp">喵惠助手V1.2.4</text>
+                     <text margin="10 5" w="*" gravity="center" textSize="18sp">喵惠助手V1.3.4</text>
 					 <text margin="10 5">喵惠助手是一款基于安卓系统的辅助脚本工具，模拟人工在手机屏幕上点击，实现自动化操作，方便、快捷、安全，没有任何病毒。</text>
 					 <text margin="10 5">部分功能是整合互联网已有开源包和工具。长期优化更新，更多功能陆续增加中……获取源码请联系我们。</text>
 
                      <text margin="10 5">先开启无障碍和悬浮窗权限，再运行软件！否则会闪退！按音量“+”键，可以结束所有正在运行的任务。</text>
 					 
-					 <text margin="10 5" textColor="#ff0000">完全免费！仅供测试和学习交流，严禁用于任何违法行为！</text>
+					 <text margin="10 5" textColor="#ff0000">完全免费！仅供测试和学习交流，严禁用于任何违规违法行为！导致的一切后果与本助手无关！</text>
 					 
 					 <card id="miaohui" w="*" h="70" margin="10 20 10 10" cardCornerRadius="2dp" cardElevation="1dp" gravity="center_vertical">
                         <vertical padding="18 8" h="auto">
                             <text text="点击下载更新喵惠助手" gravity="center" textColor="#000000" textSize="16sp"/>
-                            <text text="喵惠APP—我的页面—喵惠助手更新下载" gravity="center" textColor="#ff5050" textSize="14sp"/>
+                            <text text="喵惠APP—我的页面—咨询在线客服" gravity="center" textColor="#ff5050" textSize="14sp"/>
                         </vertical>
                     <View h="*" w="10"/>
                     </card>
-					
-					<vertical margin="10 0" gravity="center">                                
-                        <img src="https://s2.loli.net/2022/01/06/YWMuFTzB2merkal.png" w="418" h="228"/>
-				    </vertical>
 								
                     </vertical>
                 </frame>
@@ -111,13 +108,16 @@ ui.autoService.checked = auto.service != null;    });
 
 //红包//////////
 ui.tmhb.click(function(){
-app.openUrl("https://resoumen.com/u/")
+app.openUrl("https://m.tb.cn/h.ftZMHwC")
 });
 
 ui.jdhb.click(function(){
-app.openUrl("https://u.jd.com/JMgvgNd")
+app.openUrl("https://u.jd.com/JwjrLXc")
 });
 
+ui.pddhb.click(function(){
+app.openUrl("https://p.pinduoduo.com/xrMIi8eR")
+});
 
 ui.youhuiquan.click(function(){
 app.openUrl("https://a.app.qq.com/o/simple.jsp?pkgname=com.miaohui.xin")
